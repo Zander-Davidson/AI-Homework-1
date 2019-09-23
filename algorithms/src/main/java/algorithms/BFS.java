e8a7b32637b6329b593c6acdf381c0ca26b2a5e9
redna <redna@10.35.36.72>
2019-09-23 09:36:56 -0500

    added comments to BFS.search

--- algorithms/src/main/java/algorithms/BFS.java
+++ algorithms/src/main/java/algorithms/BFS.java
@@ -5,43 +5,67 @@ import java.util.HashMap;
 
 public class BFS {
 
-	public String search(String startNodeId, String goalNodeId) {
-		if (!Driver.NODES.containsKey(startNodeId) || !Driver.NODES.containsKey(goalNodeId)) {
+	/**
+	 * performs a Breadth-First Search on Driver.NODES. uses a while loop, a queue
+	 * and a visited array to keep track of search progress. outputs search results
+	 * if a goal node is found, or error messages if not
+	 * 
+	 * @param start - identifier String of the starting node
+	 * @param goal  - identifier String of the goal node
+	 * @return a String describing the search results (invalid nodes supplied; num
+	 *         nodes visited, num nodes in path, and length of path; or no goal node
+	 *         found)
+	 */
+	public String search(String start, String goal) {
+		if (!Driver.NODES.containsKey(start) || !Driver.NODES.containsKey(goal)) {
 			return "Search failure: invalid nodes.";
 		}
 
+		// shallowest node will always be pushed to the queue
 		ArrayList<String> queue = new ArrayList<String>();
+
+		// node added to visited when it is expanded
 		ArrayList<String> visited = new ArrayList<String>();
+
+		// keep track of parent of each node so that we can backtrack later to find the
+		// path to the goal node
 		HashMap<String, String> parentMap = new HashMap<String, String>();
 
-		queue.add(startNodeId);
-		String parentNodeId;
+		queue.add(start);
+		String parent;
 
+		// we loop as long as queue is not empty (in such case, we have not found a goal
+		// node and the search failed)
 		while (!queue.isEmpty()) {
-			parentNodeId = queue.remove(0);
-			visited.add(parentNodeId);
+			parent = queue.remove(0);
+			visited.add(parent);
 
-			ArrayList<String> children = Driver.NODES.get(startNodeId).getConnectionIds();
+			ArrayList<String> children = Driver.NODES.get(start).getConnectionIds();
 
-			for (String childNodeId : Driver.NODES.get(parentNodeId).getConnectionIds()) {
-				if (!visited.contains(childNodeId) && !queue.contains(childNodeId)) {
-					parentMap.put(childNodeId, parentNodeId);
+			// looping through the connections (children) of the node selected for expansion
+			for (String child : Driver.NODES.get(parent).getConnectionIds()) {
 
-					if (childNodeId.equals(goalNodeId)) {
+				// make sure it is not a repetition
+				if (!visited.contains(child) && !queue.contains(child)) {
+					parentMap.put(child, parent);
+
+					// stopping condition. now backtrack to find a path and distance back to goal
+					if (child.equals(goal)) {
 						ArrayList<String> path = new ArrayList<String>();
 						double distance = 0;
 
-						while (childNodeId != startNodeId) {
-							path.add(0, childNodeId);
-							distance += Driver.NODES.get(childNodeId).getConnections().get(parentMap.get(childNodeId));
-							childNodeId = parentMap.get(childNodeId);
+						while (child != start) {
+							path.add(0, child);
+							distance += Driver.NODES.get(child).getConnections().get(parentMap.get(child));
+							child = parentMap.get(child);
 						}
-						path.add(0, startNodeId);
-						return "Breadth-First Search from " + parentNodeId + " to " + goalNodeId
-								+ "\nNum nodes visited: " + visited.size() + "\nNum nodes on path: " + path.size()
-								+ "\nDistance (km): " + distance;
+						path.add(0, start);
+
+						return "Breadth-First Search from " + start + " to " + goal + "\nNum nodes visited: "
+								+ visited.size() + "\nNum nodes on path: " + path.size() + "\nDistance (km): "
+								+ distance;
 					} else {
-						queue.add(childNodeId);
+						queue.add(child);
 					}
 				}
 			}
